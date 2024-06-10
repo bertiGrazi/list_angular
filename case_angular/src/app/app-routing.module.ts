e7c1d8e88a8fc10cab7f5ad7577cfc840d991c90
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-06-10 12:31:56 -0300

    add item component and set router

--- case_angular/src/app/app-routing.module.ts
+++ case_angular/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
 
 const routes: Routes = [
-  {path: 'list', component: ListComponent}
+  {path: 'list', component: ListComponent},
+  {path: 'additem', component: AddItemComponent}
 ];
 
 @NgModule({
