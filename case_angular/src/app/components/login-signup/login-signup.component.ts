9e78281c7ad87db1730de5a81858a4e746719dea
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-06-10 21:46:25 -0300

    finish validtors for login user

--- case_angular/src/app/components/login-signup/login-signup.component.ts
+++ case_angular/src/app/components/login-signup/login-signup.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { signup } from '../itemsmodel';
+import { signup, login } from '../itemsmodel';
 
 @Component({
   selector: 'app-login-signup',
@@ -11,12 +11,18 @@ import { signup } from '../itemsmodel';
 export class LoginSignupComponent implements OnInit{
   isShow = false
   signUpForm!: FormGroup
+  loginForm!: FormGroup
 
   constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.signUpForm = this.formBuilder.group({
       name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+
+    this.loginForm = this.formBuilder.group({
       email: ['', Validators.required],
       password: ['', Validators.required],
     })
@@ -34,6 +40,24 @@ export class LoginSignupComponent implements OnInit{
     this.http.post<signup>("http://localhost:3000/signup", this.signUpForm.value).subscribe(res => {
       alert("Cadastro concluido com sucesso!!!!")
       this.signUpForm.reset()
+    })
+  }
+
+  loginUser() {
+    this.http.get<login[]>("http://localhost:3000/signup").subscribe(res => {
+      const user = res.find((person: any) => {
+        return person.email === this.loginForm.value.email && person.password === this.loginForm.value.password
+      })
+      if (user) {
+        alert("Pronto para logar!!!!")
+        this.loginForm.reset()
+      } else {
+        alert("Ops!!!!!! Usuário não encontrado com essas informações! Verifique se estão corretas!")
+        this.loginForm.reset()
+      }
+    }, err => {
+      alert("Ihhh! Alguma coisa está errada! Tente mais tarde!")
+      this.loginForm.reset()
     })
   }
 }
