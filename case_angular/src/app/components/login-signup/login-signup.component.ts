c57d99b3f53147950db8535ed7670a61fe10a735
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-06-10 21:35:25 -0300

    create user form with api

--- case_angular/src/app/components/login-signup/login-signup.component.ts
+++ case_angular/src/app/components/login-signup/login-signup.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { signup } from '../itemsmodel';
 
 @Component({
   selector: 'app-login-signup',
   templateUrl: './login-signup.component.html',
   styleUrls: ['./login-signup.component.css']
 })
-export class LoginSignupComponent {
+export class LoginSignupComponent implements OnInit{
   isShow = false
+  signUpForm!: FormGroup
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
 
   signUp() {
     this.isShow = true
@@ -14,5 +28,12 @@ export class LoginSignupComponent {
 
   login() {
     this.isShow = false
+  }
+
+  submitSignUp() {
+    this.http.post<signup>("http://localhost:3000/signup", this.signUpForm.value).subscribe(res => {
+      alert("Cadastro concluido com sucesso!!!!")
+      this.signUpForm.reset()
+    })
   }
 }
